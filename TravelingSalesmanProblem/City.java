93912b2736e163f42b71290e7acac46fb62eefee
yl2612 <yl2695@nyu.edu>
2015-09-24 15:04:29 -0400

    add city

---
+++ TravelingSalesmanProblem/City.java
@@ -0,0 +1,30 @@
+public class City {
+    private int x;
+    private int y;
+    private int z;
+
+    public City(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getZ() {
+        return this.z;
+    }
+
+    public double computeDistance(City city) {
+        int xDistance = this.x - city.getX();
+        int yDistance = this.y - city.getY();
+        int zDistance = this.z - city.getZ();
+        return Math.sqrt(xDistance * xDistance + yDistance * yDistance + zDistance * zDistance);
+    }
+}
