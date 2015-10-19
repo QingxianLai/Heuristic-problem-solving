e8ffd00ea9fb2190d50390d06c4329fdbf1b9fe2
yl2612 <yl2695@nyu.edu>
2015-10-18 21:56:12 -0400

    add write to file although code is so ugly...

--- AmbulancePlanning/GreedyPlan.py
+++ AmbulancePlanning/GreedyPlan.py
@@ -15,9 +15,10 @@ class GreedyPlan(object):
     def __init__(self, file_name):
         self.file_name = file_name
         self.saved_patients = []
+        self.ambulance_patients = {}
 
     def __savable(self, patient, ambulance, hospital, i):
-        return patient.time - self.__distance(patient, ambulance) > (1 + (4 - i) * 0.1) * self.__distance(patient, hospital)
+        return patient.time - self.__distance(patient, ambulance) - ambulance.current_time > (1 + (4 - i) * 0.1) * self.__distance(patient, hospital)
     
     def __distance(self, patient, ambulance):
         return abs(patient.x - ambulance.x) + abs(patient.y - ambulance.y)
@@ -25,14 +26,32 @@ class GreedyPlan(object):
     def strategy(self):
         kmeans = KMeans(self.file_name)
         hospitals = kmeans.k_means()
-        j = 1
-        for hospital in hospitals:
+        f = open("result.txt", "w")
+
+        sorted_hospitals = sorted(hospitals.keys())
+        k = 1
+        for hospital in sorted_hospitals:
+            f.write("Hospital:" + str(hospital.id) + "|" + str(hospital.x) + "," + str(hospital.y) + "," + str(hospital.ambu) + "|")
+            for i in range(hospital.ambu):
+                if i != hospital.ambu - 1:
+                    f.write(str(k) + ",")
+                else:
+                    f.write(str(k) + "\n")
+                k += 1
+        ambulance_num = k - 1
+
+        k = 1
+        f.write("\n")
+        ambulance_id = {}
+        for hospital in sorted_hospitals:
             patients = filter(lambda x: x.time >  2.3 * (abs(x.x - hospital.x) + abs(x.y - hospital.y)),hospitals[hospital])
             
             ambu_num = hospital.ambu
             ambulances = []
             for i in range(ambu_num):
-                ambulances.append(Ambulance(i, hospital))
+                ambulances.append(Ambulance(k, hospital))
+                ambulance_id[ambulances[-1].id] = ambulances[-1]
+                k += 1
             while True:
                 pre_num = len(self.saved_patients)
                 for i in range(4):
@@ -50,18 +69,31 @@ class GreedyPlan(object):
                     ambulance.current_time += self.__distance(ambulance, hospital) + 1
                     for patient in ambulance.patients:
                         if patient.time >= ambulance.current_time:
+                            if ambulance.id not in self.ambulance_patients:
+                                self.ambulance_patients[ambulance.id] = []
+                                self.ambulance_patients[ambulance.id].append(patient)
+                            else:
+                                self.ambulance_patients[ambulance.id].append(patient)
                             self.saved_patients.append(patient)
                     ambulance.patients = []
                     ambulance.x = hospital.x
                     ambulance.y = hospital.y
                 if len(self.saved_patients) == pre_num:
                     break
+        for id in range(1, ambulance_num + 1):
+            f.write("Ambulance:" + str(id) + "|" + str(ambulance_id[id].x) + "," + str(ambulance_id[id].y) + "|")
+            for i in range(len(self.ambulance_patients[id])):
+                patient = self.ambulance_patients[id][i]
+                if i != len(self.ambulance_patients[id]) - 1:
+                    f.write(str(patient.id) + "," + str(patient.x) + "," + str(patient.y) + "," + str(patient.time) + ";")
+                else:
+                    f.write(str(patient.id) + "," + str(patient.x) + "," + str(patient.y) + "," + str(patient.time) + "|" + str(ambulance_id[id].x) + "," + str(ambulance_id[id].y) + "\n")
+        f.close()
+
 
 def main():
     plan = GreedyPlan("test.txt")
     plan.strategy()
-    print plan.saved_patients
-    print len(plan.saved_patients)
 
 if __name__ == '__main__':
     main()
