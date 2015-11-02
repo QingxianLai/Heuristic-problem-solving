64ec2d78d7b14f9e28157897d1132e1c3e5eded2
yl2612 <yl2695@nyu.edu>
2015-11-02 15:47:03 -0500

    add a naive strategy for prey

--- evasion/clients/GameStrategy.py
+++ evasion/clients/GameStrategy.py
@@ -1,7 +1,7 @@
 import websocket
 import sys
 import json
-
+import math
 
 class GameStrategy(object):
     def __init__(self, role, ws, publisher):
@@ -10,6 +10,8 @@ class GameStrategy(object):
         self.ws = ws
         self.publisher = publisher
         self.first_move = True
+        self.hunter_prev_move = (0, 0)
+        self.prey_prev_move = (230, 200)
 
     def _send_to_server(self, command_dict):
         """docstring for _send_to_server"""
@@ -32,18 +34,71 @@ class GameStrategy(object):
                     a = self._get_position()
                     print a
 
-
     def prey_strategy(self):
         """docstring for preyStrategy"""
         while self.ws.connected:
             if self.first_move:
-                self._send_moving_message("N")
+                pos = self._get_position()
+                hunter_pos = pos["hunter"]
+                prey_pos = pos["prey"]
+                hunter_direction = self._get_direction(self.hunter_prev_move, hunter_pos)
+                self._if_can_be_caught_change_direction(hunter_direction, hunter_pos, prey_pos)
+                best_direction = self._get_best_move_direction(hunter_direction, hunter_pos[0] - prey_pos[0], hunter_pos[1] - prey_pos[1])
+                self._send_moving_message(best_direction)
                 self.first_move = False
+                self.hunter_prev_move = hunter_pos
             else:
                 while self.publisher.recv():
-                    self._send_moving_message("S")
-                    a = self._get_position()
-                    print a
+                    pos = self._get_position()
+                    hunter_pos = pos["hunter"]
+                    prey_pos = pos["prey"]
+                    hunter_direction = self._get_direction(self.hunter_prev_move, hunter_pos)
+                    if self._if_can_be_caught_change_direction(hunter_direction, hunter_pos, prey_pos) != None:
+                        best_direction = self._if_can_be_caught_change_direction(hunter_direction, hunter_pos, prey_pos)
+                    else:
+                        best_direction = self._get_best_move_direction(hunter_direction, hunter_pos[0] - prey_pos[0], hunter_pos[1] - prey_pos[1])
+                    self._send_moving_message(best_direction)
+                    self.first_move = False
+                    self.hunter_prev_move = hunter_pos
+
+    def _if_can_be_caught_change_direction(self, hunter_direction, hunter_pos, prey_pos):
+        if hunter_direction == "NW" or hunter_direction == "SE":
+            distance = abs(prey_pos[0] - prey_pos[1] + hunter_pos[1] - hunter_pos[0]) / math.sqrt(1 + 1)
+            if distance < 3:
+                if - hunter_pos[1] + prey_pos[1] + hunter_pos[0] > prey_pos[1]:
+                    return "SW"
+                else:
+                    return "NE"
+        else:
+            distance = abs(prey_pos[0] + prey_pos[1] - hunter_pos[1] - hunter_pos[0]) / math.sqrt(1 + 1)
+            if distance < 3:
+                if hunter_pos[1] - prey_pos[1] + hunter_pos[0] > prey_pos[1]:
+                    return "NW"
+                else:
+                    return "SE"
+
+    def _get_best_move_direction(self, hunter_direction, relative_x, relative_y):
+        if hunter_direction == 'NW':
+            if relative_x > 0 and relative_y > 0:
+                best_move = 'SE'
+            else:
+                best_move = 'NW'
+        elif hunter_direction == 'NE':
+            if relative_x < 0 and relative_y > 0:
+                best_move = 'SW'
+            else:
+                best_move = 'NE'
+        elif hunter_direction == 'SW':
+            if relative_x > 0 and relative_y < 0:
+                best_move = 'NE'
+            else:
+                best_move = "SW"
+        elif hunter_direction == 'SE':
+            if relative_x < 0 and relative_y < 0:
+                best_move = 'NW'
+            else:
+                best_move = 'SE'
+        return best_move
                     
     def _get_direction(self, pos1, pos2):
         if pos1[0] == pos2[0] and pos1[1] < pos2[1]:
@@ -60,7 +115,7 @@ class GameStrategy(object):
             return "NE"
         elif pos1[0] < pos2[0] and pos1[1] < pos2[1]:
             return "SE"
-        elif pos1[0] > pos2[0] and pos1[1] < pos2[1]:
+        else:#  pos1[0] > pos2[0] and pos1[1] < pos2[1]:
             return "SW"
 
     def _send_moving_message(self, direction=""):
@@ -99,13 +154,11 @@ def main():
         port = "1991"
         url = "ws://localhost:" + port
         ws = websocket.create_connection(url)
-        # prey = websocket.create_connection("ws://localhost:1992")
     else:
         role = 'P'
         port = "1992"
         url = "ws://localhost:" + port
         ws = websocket.create_connection(url)
-        # hunter = websocket.create_connection("ws://localhost:1991")
     strategy = GameStrategy(role, ws, publisher)
     strategy.run()
 
