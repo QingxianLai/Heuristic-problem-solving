2999e14052b14b882e9d44aa0368c0f1dbdfee3a
yl2612 <yl2695@nyu.edu>
2015-11-02 00:31:00 -0500

    fix GameStrategy.py

--- evasion/clients/GameStrategy.py
+++ evasion/clients/GameStrategy.py
@@ -4,10 +4,12 @@ import json
 
 
 class GameStrategy(object):
-    def __init__(self, role, ws):
+    def __init__(self, role, ws, publisher):
         """docstring for __init__"""
         self.role = role
         self.ws = ws
+        self.publisher = publisher
+        self.first_move = True
 
     def _send_to_server(self, command_dict):
         """docstring for _send_to_server"""
@@ -20,31 +22,91 @@ class GameStrategy(object):
 
     def hunterStrategy(self):
         """docstring for hunterStrategy"""
-        pass
+        while self.ws.connected:
+            if self.first_move:
+                self._send_to_server({"command": "M"})
+                self.first_move = False
+            else:
+                while self.publisher.recv():
+                    self._send_moving_message()
+                    a = self._get_position()
+                    print a
 
-    def preyStrategy(self):
+
+    def prey_strategy(self):
         """docstring for preyStrategy"""
-        pass
+        while self.ws.connected:
+            if self.first_move:
+                self._send_moving_message("N")
+                self.first_move = False
+            else:
+                while self.publisher.recv():
+                    self._send_moving_message("S")
+                    a = self._get_position()
+                    print a
+                    
+    def _get_direction(self, pos1, pos2):
+        if pos1[0] == pos2[0] and pos1[1] < pos2[1]:
+            return "S"
+        elif pos1[0] == pos2[0] and pos1[1] > pos2[1]:
+            return "N"
+        elif pos1[0] < pos2[0] and pos1[1] == pos2[1]:
+            return "E"
+        elif pos1[0] > pos2[0] and pos1[1] == pos2[1]:
+            return "W"
+        elif pos1[0] > pos2[0] and pos1[1] > pos2[1]:
+            return "NW"
+        elif pos1[0] < pos2[0] and pos1[1] > pos2[1]:
+            return "NE"
+        elif pos1[0] < pos2[0] and pos1[1] < pos2[1]:
+            return "SE"
+        elif pos1[0] > pos2[0] and pos1[1] < pos2[1]:
+            return "SW"
+
+    def _send_moving_message(self, direction=""):
+        if direction == "":
+            message = {"command": "M"}
+        else:
+            message = {"command": "M", "direction": direction}
+        self._send_to_server(message)
+    
+    def _get_walls(self):
+        message = {"command": "W"}
+        json_string = self._send_to_server(message)
+        obj = json.loads(json_string)
+        walls = obj["walls"]
+        return walls
+
+    def _get_position(self):
+        message = {"command": "P"}
+        json_string = self._send_to_server(message)
+        obj = json.loads(json_string)
+        return obj
 
     def run(self):
         """docstring for run"""
         if self.role == "H":
             self.hunterStrategy()
         else:
-            self.preyStrategy()
+            self.prey_strategy()
 
 
 def main():
     """docstring for main"""
-    role = "H"
-    port = "1991"
-    if len(sys.argv) > 1:
-        role = sys.argv[1]
-        if role == 'P':
-            port = "1992"
-    url = "ws://localhost:" + port
-    ws = websocket.create_connection(url)
-    strategy = GameStrategy(role, ws)
+    publisher = websocket.create_connection("ws://localhost:1990")
+    if sys.argv[1] == "H":
+        role = "H"
+        port = "1991"
+        url = "ws://localhost:" + port
+        ws = websocket.create_connection(url)
+        # prey = websocket.create_connection("ws://localhost:1992")
+    else:
+        role = 'P'
+        port = "1992"
+        url = "ws://localhost:" + port
+        ws = websocket.create_connection(url)
+        # hunter = websocket.create_connection("ws://localhost:1991")
+    strategy = GameStrategy(role, ws, publisher)
     strategy.run()
 
 if __name__ == '__main__':
--- evasion/clients/echo_client.py
+++ evasion/clients/echo_client.py
@@ -5,45 +5,63 @@ import json
 
 def main():
     """docstring for main"""
-    role = "H"
-    port = "1991"
-    if len(sys.argv) > 1:
-        role = sys.argv[1]
-        if role == 'P':
-            port = "1992"
-    url = "ws://localhost:" + port
-    ws = websocket.create_connection(url)
+    publisher = websocket.create_connection("ws://localhost:1990")
+    if sys.argv[1] == "H":
+        role = "H"
+        port = "1991"
+        url = "ws://localhost:" + port
+        ws = websocket.create_connection(url)
+        # prey = websocket.create_connection("ws://localhost:1992")
+    else:
+        role = 'P'
+        port = "1992"
+        url = "ws://localhost:" + port
+        ws = websocket.create_connection(url)
+        # hunter = websocket.create_connection("ws://localhost:1991")
+    
     print "%s, Welcome!" % role
+    test = None
 
     while True:
-        a = raw_input("command >")
-        pack = {}
-        if a == "B":
-            wall_length = raw_input("wall_lenth >")
-            wall_direct = raw_input("wall direction >")
-            pack = {"command": "B", "wall": {"length": wall_length,
-                                             "direction": wall_direct}}
-        elif a == "D":
-            wall_index = raw_input("wall index>")
-            pack = {"command": "D", "wallindex": wall_index}
-        elif a == "M":
-            if role == "P":
-                direction = raw_input("move direction>")
-                pack = {"command": "M", "direction": direction}
+        times = 1
+        if role == "H":
+            times = 2
+        for i in range(times):
+            a = raw_input("command >")
+            pack = {}
+            if a == "B":
+                wall_length = raw_input("wall_lenth >")
+                wall_direct = raw_input("wall direction >")
+                pack = {"command": "B", "wall": {"length": wall_length,
+                                                 "direction": wall_direct}}
+            elif a == "D":
+                wall_index = raw_input("wall index>")
+                pack = {"command": "D", "wallindex": wall_index}
+            elif a == "M":
+                if role == "P":
+                    direction = raw_input("move direction>")
+                    pack = {"command": "M", "direction": direction}
+                else:
+                    pack = {"command": "M"}
+            elif a == "P":
+                pack = {"command": "P"}
+            elif a == "W":
+                pack = {"command": "W"}
             else:
-                pack = {"command": "M"}
-        elif a == "P":
-            pack = {"command": "P"}
-        elif a == "W":
-            pack = {"command": "W"}
-        else:
-            print "unknown command"
-            continue
-        json_string = json.dumps(pack)
-        print "==|| message sent: %s" % json_string
-        ws.send(json_string)
-        result = ws.recv()
-        print "==|| Get message: %s" % result
+                print "unknown command"
+                continue
+            json_string = json.dumps(pack)
+            print "==|| message sent: %s" % json_string
+            ws.send(json_string)
+            test = 1
+
+            if a == "P" or a == "W":
+                result = ws.recv()
+                print "==|| Get message: %s" % result
+            # elif test != None:
+            #     result = publisher.recv()
+            #     print result
+                
 
 if __name__ == '__main__':
     main()
